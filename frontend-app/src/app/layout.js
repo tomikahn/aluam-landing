8cf53ca6a49f73961bc8045197e197c144562abe
kobra <tomikahn2000@gmail.com>
2024-08-20 16:28:28 -0300

    deploy update

--- frontend-app/src/app/layout.js
+++ frontend-app/src/app/layout.js
@@ -6,13 +6,17 @@ const montserrat = Montserrat({ subsets: ['latin'] })
 export const metadata = {
   title: 'ALUAM',
   description: 'En ALUAM nos dedicamos a la fabricación y comercialización de aberturas de aluminio, PVC y vidrios.',
-  favicon: '/favicon.ico',
 };
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta property="og:image" content="/favicon.ico" />
+
+      <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+      </head>
       <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   )
